9de765e7e761b1af438c41fee8571a38d2255c5e
Wolodya <vovatarasov6991@gmail.com>
2018-12-18 17:19:09 +0200

    add services to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MonitoringService } from 'src/services/monitoring.service';
+import { TargetService } from 'src/services/target.service';
+import { SessionService } from 'src/services/session.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [MonitoringService,TargetService,SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
